7380340584021b6bb2a98b0f2d8e34d50474453d
splogamurugan1 <sploga.murugan@gmail.com>
2016-09-05 22:06:48 +0530

    Added server side express files

--- client/angrum/app/services/appUtils.js
+++ client/angrum/app/services/appUtils.js
@@ -1,8 +1,16 @@
-angular.module('angrum.common').factory('appUtils', function() {
+angular.module('angrum.common').factory('appUtils', function(appModel) {
     return {
         title: function(title) {
             return title.charAt(0).toUpperCase() + title.slice(1);
         },
+        getMenu: function() {
+            var menu={};
+            appModel.getData('server/modules.json').then(function(d) {
+                console.log(d);
+                menu.modules = d.data;
+            });
+            return menu;
+        }
         
     }
 });
